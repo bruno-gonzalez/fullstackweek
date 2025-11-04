aac67a72c644618529cb99095694c954a01e52aa
bruno-gonzalez <brunotorres0717@gmail.com>
2025-11-03 23:29:12 -0300

    feat- add home message

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 const config = {
-  plugins: [
-    ["@tailwindcss/postcss", { config: "./tailwind.config.ts" }],
-  ],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
 
 export default config;
