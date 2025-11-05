891e95de12ada8cbadab5917c76e72d066c0eb64
bruno-gonzalez <brunotorres0717@gmail.com>
2025-11-05 12:56:10 -0300

    feat:  add barbershopitem

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,6 +1,8 @@
+import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
 
 interface BarbershopItemProps {
@@ -10,25 +12,34 @@ interface BarbershopItemProps {
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl py-0">
-            <CardContent className="p-0">
-            <div className="px-1">
-                <Image
-                    src={barbershop.imageUrl}
-                    alt={barbershop.name}
-                    height={0}
-                    width={0}
-                    sizes="100vw"
-                    className="h-[159px] w-full rounded-2xl"
-                />
-                <h2 className="font-bold mt-2 text-sm">{barbershop.name}</h2>
-                <p className="text-xs text-gray-400 overflow-hidden text-ellipsis text-nowrap">
-                {barbershop.address}
-                </p>
-                    <Button className="w-full mt-3" variant={"secondary"}>
-                    Reservar
-                    </Button>
-            </div>
-        </CardContent>
+      <CardContent className="p-0">
+        <div className="w-full h-[159px] relative">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge variant={'secondary'} className="flex items-center justify-center opacity-90">
+            <StarIcon className="fill-primary text-primary" size={12}/>
+            <span>5,0</span>
+          </Badge>
+          </div>
+          <Image
+            src={barbershop.imageUrl}
+            alt={barbershop.name}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-[159px] w-full rounded-2xl"
+          />
+        </div>
+
+        <div className="px-2 pb-2">
+          <h2 className="font-bold mt-2 text-sm">{barbershop.name}</h2>
+          <p className="text-xs text-gray-400 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.address}
+          </p>
+          <Button className="w-full mt-3" variant={"secondary"}>
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
     </Card>
   );
 };
